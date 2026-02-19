22ddc281fa8d792d4dd0891a87122d46a71a3f00
GlitchRaven <errorglitchraven@gmail.com>
2026-02-20 00:48:25 +0530

    STarted with something

--- database/db_connections.py
+++ database/db_connections.py
@@ -1,2 +1,11 @@
 import sqlite3 as sql
 from config import DATABASE_PATH
+
+def get_connection():
+    try:
+        conn = sql.connect(DATABASE_PATH)
+        conn.execute("PRAGMA foreign_keys = ON;")
+        return conn
+    except sql.Error as e:
+        print(f"Error connecting to database: {e}")
+        return None
