3751c4f3ea605d66a2b803a732abcdaf7475c342
GlitchRaven <errorglitchraven@gmail.com>
2026-02-22 23:19:54 +0530

    Working on adding functionality

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+from unittest import case
+
 from database.init_db import init_db
 
-init_db()
+FIRST_RUN_ACTIONS = [
+    "Login",
+    "Register",
+    "Exit"
+]
+
+def initialize_database():
+    init_db()
+
+def main():
+    initialize_database()
+    print("Welcome to the CLI Library Management System!")
+    for i in range(FIRST_RUN_ACTIONS):
+        print(f"{i+1}. {FIRST_RUN_ACTIONS[i]}")
+    
+    while True:
+        choice = input("Please select an option: ")
+        if choice == "1":
+            print("Login functionality is not implemented yet.")
+        elif choice == "2":
+            print("Registration functionality is not implemented yet.")
+        elif choice == "3":
+            print("Exiting the system. Goodbye!")
+            break
+        else:
+            print("Invalid option. Please try again.")
+            exit(1)
+
+    
+
+
+def initial_setup():
+    main()
